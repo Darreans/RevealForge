0c3280864338741da138d72c90feaee9acf220dd
Darrean <95978260+Darreans@users.noreply.github.com>
2025-05-26 03:19:05 -0400

    Add files via upload

--- Plugin.cs
+++ Plugin.cs
@@ -1,56 +1,71 @@
 ﻿using BepInEx;
 using BepInEx.Unity.IL2CPP;
+using BepInEx.Logging;
 using HarmonyLib;
 using System.Reflection;
-using RevealForge.Utils;
 using RevealForge.Hooks;
 using System;
-using VampireCommandFramework;
-using RevealForge.Middlewares;
+using VampireCommandFramework; 
+using RevealForge.Middlewares; 
 
 namespace RevealForge
 {
     [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
-    [BepInDependency("gg.deca.VampireCommandFramework")]
+    [BepInDependency("gg.deca.VampireCommandFramework")] 
     public class Plugin : BasePlugin
     {
         private Harmony _harmony;
+        public static ManualLogSource LoggerInstance { get; private set; }
 
         public override void Load()
         {
-            VWorld.Initialize();
+            LoggerInstance = Log;
+
+            LoggerInstance.LogInfo($"Loading {MyPluginInfo.PLUGIN_NAME} v{MyPluginInfo.PLUGIN_VERSION}...");
+
             InitializeHooks();
             RegisterVCFMiddleware();
             ApplyHarmonyPatches();
+
+            LoggerInstance.LogInfo($"Plugin {MyPluginInfo.PLUGIN_NAME} successfully loaded!");
         }
 
         private void InitializeHooks()
         {
+            LoggerInstance.LogInfo("Initializing Harmony hooks...");
             try
             {
-                GiveDebugEventHook.Initialize();
-                CreateJewelDebugEventV2Hook.Initialize();
-                CreateLegendaryWeaponEventHook.Initialize();
-                GenerateLegendaryWeaponEventHook.Initialize();
-                GenerateBloodPotionAdminEventHook.Initialize();
-                ConsumeBloodAdminEventHook.Initialize();
-                ChangeHealthOfClosestToPositionEventHook.Initialize();
-                CompleteCurrentAchievementEventHook.Initialize();
-                CombatPresetEventHook.Initialize();
-                InventoryCommandsHook.Initialize();
-                RenameEventHook.Initialize();
+                GiveDebugEventHook.Initialize(LoggerInstance);
+                CreateJewelDebugEventV2Hook.Initialize(LoggerInstance);
+                CreateLegendaryWeaponEventHook.Initialize(LoggerInstance);
+                GenerateLegendaryWeaponEventHook.Initialize(LoggerInstance);
+                GenerateBloodPotionAdminEventHook.Initialize(LoggerInstance);
+                ConsumeBloodAdminEventHook.Initialize(LoggerInstance);
+                ChangeDurabilityEventHook.Initialize(LoggerInstance);
+                ChangeHealthOfClosestToPositionEventHook.Initialize(LoggerInstance);
+                CompleteCurrentAchievementEventHook.Initialize(LoggerInstance);
+
+                LoggerInstance.LogInfo("All Harmony hook initializations called.");
             }
-            catch (Exception) {  }
+            catch (Exception e)
+            {
+                LoggerInstance.LogError($"An error occurred during Harmony hook initialization: {e}");
+            }
         }
 
         private void RegisterVCFMiddleware()
         {
+            LoggerInstance.LogInfo("Attempting to register VCF middlewares...");
             try
             {
-                var adminAnnounceMiddleware = new AdminAnnounceMiddleware();
-                CommandRegistry.Middlewares.Add(adminAnnounceMiddleware);
+                var adminAnnounceMiddleware = new AdminAnnounceMiddleware(LoggerInstance);
+                CommandRegistry.Middlewares.Add(adminAnnounceMiddleware); 
+                LoggerInstance.LogInfo("AdminAnnounceMiddleware added to VCF's middleware list.");
             }
-            catch (Exception) { }
+            catch (Exception e)
+            {
+                LoggerInstance.LogError($"Error registering VCF middleware: {e}");
+            }
         }
 
         private void ApplyHarmonyPatches()
@@ -59,8 +74,12 @@ namespace RevealForge
             {
                 _harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
                 _harmony.PatchAll(Assembly.GetExecutingAssembly());
+                LoggerInstance.LogInfo($"Plugin {MyPluginInfo.PLUGIN_NAME} patches applied!");
             }
-            catch (Exception) { }
+            catch (Exception e)
+            {
+                LoggerInstance.LogError($"An error occurred while applying Harmony patches: {e}");
+            }
         }
 
         public override bool Unload()
@@ -68,13 +87,16 @@ namespace RevealForge
             if (_harmony != null)
             {
                 _harmony.UnpatchSelf();
+                LoggerInstance.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} patches are Unloaded!");
             }
-            try
-            {
-                CommandRegistry.Middlewares.RemoveAll(m => m is AdminAnnounceMiddleware);
-            }
-            catch (Exception) {  }
             return true;
         }
+    }
+
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "RevealForge";
+        public const string PLUGIN_NAME = "RevealForge Event Announcer";
+        public const string PLUGIN_VERSION = "1.0.0"; 
     }
 }
